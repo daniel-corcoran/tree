926602215cacb046f0d2f14bcbff856d2299bf75
daniel-corcoran <corcordp@mail.uc.edu>
2020-05-12 23:15:48 -0400

    Added git features

--- tools/update.py
+++ tools/update.py
@@ -1,5 +1,5 @@
 import os
 def update():
-    r = os.system('git pull')
-    print(r)
+    p = os.system('echo %s|sudo -S %s' % ('tree2020', 'git pull'))
+    print(p)
     # Git pulled, now we want to
