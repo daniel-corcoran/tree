1e26573151b73eda4bc8b7b92a3637ae7ce6540c
daniel-corcoran <corcordp@mail.uc.edu>
2020-05-13 20:06:57 -0400

    Added modularization of packages

--- boot.py
+++ boot.py
@@ -1,123 +1,96 @@
-from app.motion_detection import SingleMotionDetector
 from waitress import serve
-from flask import *
 from flask import render_template
-from flask import request
 from flask import Response
 import threading
-import datetime
-import imutils
-import time
-import cv2
 from app import app
 from tools.power import reboot, power_off
 from tools.update import update
-from imutils.video import VideoStream
 from tools import LED
+import importlib
+import os
+from werkzeug.utils import secure_filename
+
+from flask import flash, request, redirect, url_for
+UPLOAD_FOLDER = 'database/tmp'
+ALLOWED_EXTENSIONS = {'tree'}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Load default app from database
+with open('database/default_app') as f:
+    default_app = f.readline()
+print("Default app: {}".format(default_app))
+
+my_program = importlib.import_module('programs.{}.main'.format(default_app))
 
 outputFrame = None
-lock = threading.Lock()
 
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
+def list_apps():
+    list_of_apps = os.listdir('programs')
+    new_l = []
+    for x in list_of_apps:
+        if x != "__pycache__":
+            new_l.append(x)
+    return new_l
 
-
-def detect_motion(frameCount):
-    # grab global references to the video stream, output frame, and
-    # lock variables
-    global vs, outputFrame, lock
-
-    # initialize the motion detector and the total number of frames
-    # read thus far
-    md = SingleMotionDetector(accumWeight=0.1)
-    total = 0
-
-    # loop over frames from the video stream
-    while True:
-        # read the next frame from the video stream, resize it,
-        # convert the frame to grayscale, and blur it
-        frame = vs.read()
-        frame = imutils.resize(frame, width=400)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7, 7), 0)
-
-        # grab the current timestamp and draw it on the frame
-        timestamp = datetime.datetime.now()
-        cv2.putText(frame, timestamp.strftime(
-            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
-        # if the total number of frames has reached a sufficient
-        # number to construct a reasonable background model, then
-        # continue to process the frame
-        if total > frameCount:
-            # detect motion in the image
-            motion = md.detect(gray)
-
-            # cehck to see if motion was found in the frame
-            if motion is not None:
-                # unpack the tuple and draw the box surrounding the
-                # "motion area" on the output frame
-                (thresh, (minX, minY, maxX, maxY)) = motion
-                cv2.rectangle(frame, (minX, minY), (maxX, maxY),
-                              (0, 0, 255), 2)
-
-        # update the background model and increment the total number
-        # of frames read thus far
-        md.update(gray)
-        total += 1
-
-        # acquire the lock, set the output frame, and release the
-        # lock
-        with lock:
-            outputFrame = frame.copy()
+@app.route("/IRoff")
+def irOFF():
+    LED.IRoff()
+    return init_config()
 
 
-def generate():
-    # grab global references to the output frame and lock variables
-    global outputFrame, lock
-
-    # loop over frames from the output stream
-    while True:
-        # wait until the lock is acquired
-        with lock:
-            # check if the output frame is available, otherwise skip
-            # the iteration of the loop
-            if outputFrame is None:
-                continue
-
-            # encode the frame in JPEG format
-            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-
-            # ensure the frame was successfully encoded
-            if not flag:
-                continue
-
-        # yield the output frame in the byte format
-        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
-               bytearray(encodedImage) + b'\r\n')
+@app.route("/IRon")
+def irON():
+    LED.IRon()
+    return init_config()
 
 
 @app.route("/video_feed")
 def video_feed():
-    return Response(generate(),
+    return Response(my_program.generate(),
                     mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 @app.route("/update")
 def update_helper():
+    # TODO: If an update has been applied, restart the device. Ask the user first. Otherwise, return "No updates"
     LED.blue()
     update()
     LED.green()
     return init_config()
 
 
-@app.route("/upload")
+@app.route("/upload", methods=['GET', 'POST'])
 def upload():
     LED.blue()
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files['file']
+        print(file)
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        else:
+            print("Not allowed")
+        return render_template("upload.html")
+    # Save uploaded files to /database/tmp/*.tree
+    # Extract template file(s) to /app/templates/*/template.html
+    # Extract everything else to /programs/*/
+    # That's it!!!
+
+
     # user has uploaded a new file. We need to parse and install it.
-    ...
-    # TODO
 
 
 @app.route("/reboot")
@@ -135,26 +108,27 @@ def power_off_helper():
 
 @app.route("/")
 def home_page():
-    return render_template('view.html')
+    return render_template('{}/template.html'.format(default_app))
 
-
-# Initialize flask server on boot
 
 @app.route('/config')
 def init_config():
-    return render_template('config.html')
+    x = list_apps()
+    print(x)
+    return render_template('config.html', list_apps = x)
 
 
 @app.route('/view')
 def init_view():
-    return render_template('view.html')
+    return render_template('{}/template.html'.format(default_app))
 
 
 if __name__ == '__main__':
     LED.green()
 
     # Start the current application as a daemon process
-    t = threading.Thread(target=detect_motion, args=(32,))
+    print("Initializing application thread.")
+    t = threading.Thread(target=my_program.thread, args=(32,))
     t.daemon = True
     t.start()
 
--- tools/LED.py
+++ tools/LED.py
@@ -1,24 +1,50 @@
-from periphery import GPIO
+try:
+    from periphery import GPIO
 
 
-r = GPIO(77, "out")
-g = GPIO(73, "out")
-b = GPIO(138, "out")
+    r = GPIO(77, "out")
+    g = GPIO(73, "out")
+    b = GPIO(138, "out")
+    ir = GPIO(141, "out")
 
-def red():
-        print("R")
-        r.write(True)
-        g.write(False)
-        b.write(False)
+    def red():
+            print("R")
+            r.write(True)
+            g.write(False)
+            b.write(False)
 
-def green():
-        print("G")
-        r.write(False)
-        g.write(True)
-        b.write(False)
+    def green():
+            print("G")
+            r.write(False)
+            g.write(True)
+            b.write(False)
 
-def blue():
-        print("B")
-        r.write(False)
-        g.write(False)
-        b.write(True)
+    def blue():
+            print("B")
+            r.write(False)
+            g.write(False)
+            b.write(True)
+
+    def IRon():
+        ir.write(True)
+
+    def IRoff():
+        ir.write(False)
+except:
+    print("Using dummy LED controls")
+    def red():
+        ...
+
+    def green():
+        ...
+
+
+    def blue():
+        ...
+
+
+    def IRon():
+        ...
+
+    def IRoff():
+        ...
