da55124579029dc991c89bd9f7563bfae7c4f6d5
daniel-corcoran <corcordp@mail.uc.edu>
2020-05-13 21:51:22 -0400

    Added modularization of packages

--- tools/update.py
+++ tools/update.py
@@ -1,5 +1,5 @@
 import os
 def update():
-    p = os.system('echo %s|sudo -S %s' % ('tree2020', 'git pull'))
-    print(p)
+    p = os.system('echo %s|sudo -S %s' % ('UCincy2020!', 'git pull'))
+    print("P: " , p)
     # Git pulled, now we want to
