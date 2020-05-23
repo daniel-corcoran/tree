75d15ba763ae84547727f857d61daa00075d538e
daniel-corcoran <corcordp@mail.uc.edu>
2020-05-23 18:45:27 -0400

    Added pose detection as official application

---
+++ programs/pose_detect/main.py
@@ -0,0 +1,39 @@
+import os
+import numpy as np
+from PIL import Image
+from pose_engine import PoseEngine
+import imutils
+import cv2
+import datetime
+import time
+from imutils.video import VideoStream
+import svgwrite
+
+# TODO: Implement a generator function that returns serialized
+# images to the webpage.
+
+
+
+
+
+vc = VideoStream(src=0).start()
+#vs = VideoStream(usePiCamera=False).start()
+time.sleep(2.0)
+
+engine = PoseEngine('models/mobilenet/posenet_mobilenet_v1_075_481_641_quant_decoder_edgetpu.tflite')
+
+def generator():
+    while True:
+        svg_canvas = svgwrite.Drawing('', size=(640, 480))
+        frame = vc.read()
+        frame = imutils.resize(frame, width=640, height=480)
+        poses, inference_time = engine.DetectPosesInImage(np.uint8(frame))
+        print('Inference time: %.fms' % inference_time)
+        for pose in poses:
+            if pose.score < 0.4: continue
+            print('\nPose Score: ', pose.score)
+            for label, keypoint in pose.keypoints.items():
+                print(' %-20s x=%-4d y=%-4d score=%.1f' %
+                      (label, keypoint.yx[1], keypoint.yx[0], keypoint.score))
+
+
