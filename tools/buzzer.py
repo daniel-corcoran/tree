391970767d748bcc070ddbfc24aa35646f322896
daniel-corcoran <corcordp@mail.uc.edu>
2020-05-27 02:39:04 -0400

    Air guitar app

--- tools/buzzer.py
+++ tools/buzzer.py
@@ -1,14 +1,19 @@
 try:
     from periphery import PWM
+    import time
     pwm = PWM(2, 0)
-    pwm.frequency = 1
-    pwm.duty_cycle = 1
+    pwm.frequency = 1e3
+    pwm.duty_cycle = 0.9
     def enable_buzzer():
         pwm.enable()
     def disable_buzzer():
         pwm.disable()
     def set_freq(freq):
         pwm.frequency = freq
+    def cooltone():
+        for x in range(10):
+            pwm.frequency = (x * 100) + 100
+            time.sleep(0.15)
 
 except:
     print("Could not import periphery lib.")
