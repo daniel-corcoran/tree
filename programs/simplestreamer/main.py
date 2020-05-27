d3150f4b970464194e182b71cddf4c20327e1729
daniel-corcoran <corcordp@mail.uc.edu>
2020-05-27 11:53:09 -0400

    Trying to improve streaming latency

--- programs/simplestreamer/main.py
+++ programs/simplestreamer/main.py
@@ -8,15 +8,19 @@ from imutils.video import VideoStream
 # Why is this so slow?
 
 def generate():
-    vc = VideoStream(src=0).start()
+    cap = cv2.VideoCapture(0)
     time.sleep(2.0)
 
     """Video streaming generator function."""
     while True:
-        frame = vc.read()
+        ret, frame = cap.read()
         frame = imutils.resize(frame, width=400) # To save bandwidth?
         cv2.imshow("frame", frame)
         #by = cv2.imencode('.jpg', frame)[1].tostring()
         #yield (b'--frame\r\n'
         #       b'Content-Type: image/jpeg\r\n\r\n' + by + b'\r\n')
 
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
