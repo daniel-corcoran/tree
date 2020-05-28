63dd9ff854207f6042562d51ce890ba21df4a3b2
daniel-corcoran <corcordp@mail.uc.edu>
2020-05-27 20:14:39 -0400

    Improving openCV latency

--- programs/simplestreamer/main.py
+++ programs/simplestreamer/main.py
@@ -1,28 +1,27 @@
 # import the necessary packages
 import imutils
 import cv2
-import datetime
 import time
-from imutils.video import VideoStream
-
+import acapture
 # Why is this so slow?
 
 def generate():
-    cap = cv2.VideoCapture(0)
+    cap = acapture.open(0)
     time.sleep(2.0)
 
     """Video streaming generator function."""
     while True:
         start = time.time()
-        ret, frame = cap.read()
-        end = time.time()
-        print("capture time: {}".format(end - start))
+        check, frame = cap.read()
+        if check:
+            end = time.time()
+            print("capture time: {}".format(end - start))
 
-        start = time.time()
-        frame = imutils.resize(frame, width=100) # To save bandwidth?
-        #cv2.imshow("frame", frame)
-        by = cv2.imencode('.jpg', frame)[1].tostring()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + by + b'\r\n')
+            start = time.time()
+            frame = imutils.resize(frame, width=100) # To save bandwidth?
+            #cv2.imshow("frame", frame)
+            by = cv2.imencode('.jpg', frame)[1].tostring()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + by + b'\r\n')
 
         print("Process and display time: {}".format(end - start))
