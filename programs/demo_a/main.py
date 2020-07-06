2d87aa5796f3f2c8a47e5165ab40500670359a30
daniel-corcoran <corcordp@mail.uc.edu>
2020-07-05 23:53:05 -0400

    Added installation and uninstallation funcs

---
+++ programs/demo_a/main.py
@@ -0,0 +1,27 @@
+# import the necessary packages
+import imutils
+import cv2
+import time
+import acapture
+# Why is this so slow?
+
+def generate():
+    cap = acapture.open(0)
+    time.sleep(2.0)
+
+    """Video streaming generator function."""
+    while True:
+        start = time.time()
+        check, frame = cap.read()
+        if check:
+            end = time.time()
+            print("capture time: {}".format(end - start))
+
+            start = time.time()
+            frame = imutils.resize(frame, width=720) # To save bandwidth?
+            #cv2.imshow("frame", frame)
+            by = cv2.imencode('.jpg', frame)[1].tostring()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + by + b'\r\n')
+
+        print("Process and display time: {}".format(end - start))
